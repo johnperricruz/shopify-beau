6c4f303cee397974208183638e8711022ba67e0d
John Perri Cruz <jcruz@optimizex.com>
2018-07-13 12:18:33 -0700

    Updates : 7/13/18 commits - Review Tabs - Header Navigation current class - Template Adjustments

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,23 @@
 $(function(){
   console.log("Loaded.................100%");
+  
   $('.loading').fadeOut();
+  openReviewTab();
 });
 
+function openReviewTab(){
+    $("a#review").click(function(e){
+		console.log(e);
+        e.preventDefault(); //Prevents hash to be appended at the end of the current URL.
+      
+      	$("div#nav-tab a").removeClass('active show');
+        $("a#nav-review-tab").addClass('active show'); 
+      
+        $(".tab-pane").removeClass('active show');
+        $("div#nav-review").addClass('active show');
+      
+        $('html, body').animate({
+            scrollTop: $("div#nav-tab").offset().top
+        }, 1000); //Change to whatever you want, this value is in milliseconds.
+    });
+}
